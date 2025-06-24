97291ce944ee285d3f693d08dc4a2fcbfc0edd89
pandya.bhavesh <pandya.bhavesh@gmail.com>
2025-06-24 15:29:51 +1000

    Added Appsettings.json

--- name-sorter/Program.cs
+++ name-sorter/Program.cs
@@ -1,43 +1,52 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using name_sorter;
-using name_sorter_application.Interface;
-using name_sorter_application.BusinessLogic.Services;
-using name_sorter_application.BusinessLogic.Helper;
 using Serilog;
 using name_sorter_application;
 
-Log.Logger = new LoggerConfiguration()
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Information()   // global threshold
             .Enrich.FromLogContext()
             .WriteTo.Console(
                 outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
             .CreateLogger();
 
-using IHost host = Host.CreateDefaultBuilder(args)
-    .UseSerilog()
-    .ConfigureServices((context, services) =>
-    {
-        services.AddNameSorterApplicationServices();
-        services.AddSingleton<App>();
-    })
-    .Build();
+        using IHost host = Host.CreateDefaultBuilder(args)
+            .UseSerilog((context, services, loggerConfiguration) =>
+            {
+                loggerConfiguration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext();
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddNameSorterApplicationServices();
+                services.AddTransient<App>();
+            })
+            .Build();
 
-using var scope = host.Services.CreateScope();
-var services = scope.ServiceProvider;
+        using var scope = host.Services.CreateScope();
+        var services = scope.ServiceProvider;
 
-try
-{
-    services.GetRequiredService<App>().Run(args);
-}
-catch (Exception ex)
-{
-    Log.Error(ex, $"An error occurred while running the application. Error: {ex.Message}");
-    return;
-}
-finally
-{
-    Log.CloseAndFlush();
+        try
+        {
+            services.GetRequiredService<App>().Run(args);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, $"An error occurred while running the application. Error: {ex.Message}");
+            return;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
 }
 
 
